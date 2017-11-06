22745f880e8adddc31fec4a2d128d17b9a4a2fb7
Boadzie <boadziedaniel43@gmail.com>
2017-11-05 19:10:03 -0800

    welcome to the Estore app

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+               description:
+                   %{<p>
+                      <em>Native Apps, Multiple Platforms</em>
+                      Answer the question “Can we build this for ALL the devices?” with a
+                      resounding YES. This book will help you get there with a real-world
+                      introduction to seven platforms, whether you’re new to mobile or an
+                      experienced developer needing to expand your options. Plus, you’ll find
+                      out which cross-platform solution makes the most sense for your needs.
+                    </p>},
+               image_url: 'kids.png',
+               price: 26.00)
+
+Product.create(title: 'Talkng smouth',
+               description:
+                   %{<p>
+<em>Native Apps, Multiple Platforms</em>
+   If you choose to download this file, make sure that the timestamp on the file is
+updated. If the timestamp isn’t updated, Rails won’t pick up the changes until
+the server is restarted
+</p>},
+               image_url: 'talk.png',
+               price: 28.00)
